b4984d3c8e4d194df254cb564340164d12adf99f
noodles <a23andreafr@iessanclemente.net>
2024-05-12 15:06:51 +0200

    final

--- src/main/CitasMedicas.java
+++ src/main/CitasMedicas.java
@@ -12,20 +12,32 @@ public class CitasMedicas {
 
     // TODO: Método para agendar una cita
     public void agendarCita(String fechaHora) {
+        calendario.add(fechaHora);
     }
 
     // TODO: Método para cancelar una cita
     public void cancelarCita(String fechaHora) {
-       
+       for (int i = 0; i < calendario.size(); i++) {
+            if(calendario.get(i).equals(fechaHora)){
+                calendario.remove(i);
+            }
+       }
     }
 
     // TODO: Método para reprogramar una cita
     public void reprogramarCita(String fechaHoraAntigua, String fechaHoraNueva) {
-       
+        for (int i = 0; i < calendario.size(); i++) {
+            if(calendario.get(i).equals(fechaHoraAntigua)){
+                calendario.remove(i);
+                calendario.add(i, fechaHoraNueva);
+            }
+       }
     }
 
     // TODO: Método para ver el calendario de citas
     public void verCalendario() {
-        
+        for (int i = 0; i < calendario.size(); i++) {
+            System.out.println(i + ") " + "Cita del " + calendario.get(i));
+        }
     }
 }
